d860ebeb8e77f9d6bb49f94ddf30dcc7e2d9779e
Mohammadreza Amini <mohammadreza99a@yahoo.com>
2020-07-30 19:49:45 +0200

    added drop down for search

--- src/components/layout/SearchForm.vue
+++ src/components/layout/SearchForm.vue
@@ -8,14 +8,43 @@
           type="search"
           v-model="title"
           placeholder="Search..."
+          @keyup.esc="onEscKey"
+          @keyup="updateDropdown"
         />
+        <div
+          uk-dropdown
+          v-if="title !== ''"
+          :class="{ 'uk-open': title !== '' }"
+        >
+          <ul
+            v-if="
+              typeof searched !== 'undefined' ||
+                typeof searched.results !== 'undefined' ||
+                searched.results.length !== 0
+            "
+            class="uk-list uk-list-striped"
+          >
+            <li
+              @click="goToMovieInfoPage(search.id)"
+              v-for="search in searched.results"
+              :key="search.id"
+              class="movieBtn"
+            >
+              <span v-if="search.name">{{ search.name }}</span>
+              <span v-else-if="search.title">{{ search.title }}</span>
+            </li>
+          </ul>
+          <ul v-else class="uk-list uk-list-striped">
+            <li>Loading...</li>
+          </ul>
+        </div>
       </form>
     </div>
   </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import { mapActions, mapGetters } from 'vuex';
 
 export default {
   name: 'SearchForm',
@@ -33,6 +62,33 @@ export default {
       this.title = '';
       this.$router.push('Search');
     },
+    onEscKey() {
+      // clearing the input when ESC key is pressed
+      this.title = '';
+    },
+    updateDropdown() {
+      this.fetchSearchedMovie(this.title);
+    },
+    goToMovieInfoPage(id) {
+      this.title = '';
+      this.$router.push({
+        name: 'MovieInfo',
+        params: { id },
+      });
+    },
+  },
+
+  computed: {
+    ...mapGetters({
+      searched: 'getSearched',
+    }),
   },
 };
 </script>
+
+<style scoped>
+.movieBtn {
+  cursor: pointer;
+  text-align: center;
+}
+</style>
